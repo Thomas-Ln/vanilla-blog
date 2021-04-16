a3e8acfa347b029281641ef2ccbebf9021c11a16
Thomas-Ln <thomas-ln@laposte.net>
2021-04-16 19:22:36 +0200

    Modify blog.sql columns createdAt for created_at

--- blog.sql
+++ blog.sql
@@ -10,14 +10,14 @@ CREATE TABLE article (
   author varchar(30) NOT NULL,
   title varchar(255) NOT NULL,
   content text NOT NULL,
-  createdAt datetime NOT NULL
+  created_at datetime NOT NULL
 );
 
 CREATE TABLE comment (
   id smallint UNSIGNED AUTO_INCREMENT NOT NULL PRIMARY KEY,
   author varchar(30) NOT NULL,
   content text NOT NULL,
-  createdAt datetime NOT NULL,
+  created_at datetime NOT NULL,
   article_id smallint(6) UNSIGNED NOT NULL
 );
 
